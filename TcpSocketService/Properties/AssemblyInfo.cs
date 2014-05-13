0576adafa2e8e03d5b514ddc940439afa5e39ae2
Marcin Badurowicz <ktos@ktos.info>
2014-05-13 20:40:36 +0200

    added version information

--- TcpSocketService/Properties/AssemblyInfo.cs
+++ TcpSocketService/Properties/AssemblyInfo.cs
@@ -6,15 +6,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TcpSocketService")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("TcpSocketService for Windows")]
+[assembly: AssemblyDescription("Generic classes for socket-based communication")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TcpSocketService")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © ktos.info 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -26,5 +25,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.7.*")]
+[assembly: AssemblyFileVersion("1.7.0.0")]
